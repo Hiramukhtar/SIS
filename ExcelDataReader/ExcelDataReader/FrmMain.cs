ef691ac6cc70599fe2f7b3166f5a352029b88b29
Hiramukhtar <32242710+Hiramukhtar@users.noreply.github.com>
2017-12-15 10:25:55 -0800

    importing project

---
+++ ExcelDataReader/ExcelDataReader/FrmMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelDataReader
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to Exit?","Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)== DialogResult.Yes)
+            {
+                Application.Exit();
+                
+            }
+        }
+    }
+}
